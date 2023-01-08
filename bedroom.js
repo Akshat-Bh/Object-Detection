8cda621e06bc9cc6315ab21d76b0606222f7acfd
Akshat-Bh <118525576+Akshat-Bh@users.noreply.github.com>
2023-01-08 13:20:18 +0530

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -9,14 +9,17 @@ function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
-function draw()
+
+function gotResult(error, results)
 {
-    image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
+
